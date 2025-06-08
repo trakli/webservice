65e0f6b92a908d5b7dfd004b1de06cc65fdc2291
Kofi <kofimokome10@gmail.com>
2025-06-08 09:35:16 +0100

    feat: Add type to Party

--- database/migrations/2024_07_20_125104_create_parties_table.php
+++ database/migrations/2024_07_20_125104_create_parties_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('parties', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('type')->nullable();
             $table->text('description')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
