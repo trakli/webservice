e50beb5442dfaa73c994ead81f0402ad10a707ec
nfebe <fenn25.fn@gmail.com>
2025-06-16 22:01:07 +0100

    refactor: Move plugin system to dedicated package
    - Moved plugin system to whilesmart/laravel-plugin-engine package
    - Updated AppServiceProvider to use the new package's service provider
    - Removed old plugin system files that were moved to the package
    - Updated composer.json to require the new package
    - Cleaned up cached configuration files

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Trakli Webservice'),
 
     /*
     |--------------------------------------------------------------------------
@@ -168,7 +168,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\PluginServiceProvider::class,
     ])->toArray(),
 
     /*
