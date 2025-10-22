0e68d1d99623d24b63b91942705d1da0b8725f09
Kofi <kofimokome10@gmail.com>
2025-10-22 21:15:30 +0100

    feat: Add server  info endpoint
    # Conflicts:
    #	routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\API\v1\PartyController;
 use App\Http\Controllers\API\v1\TransactionController;
 use App\Http\Controllers\API\v1\TransferController;
 use App\Http\Controllers\API\v1\UserController;
+use App\Http\Controllers\API\v1\VersionController;
 use App\Http\Controllers\API\v1\WalletController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,25 +24,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Stateless public routes are now automatically registered by the user-authentication package
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('info', [VersionController::class, 'getServerInfo']);
 
-// Stateful authenticated routes
-Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function () {
-    Route::group(['middleware' => ['request.body.json']], function () {
-        Route::get('/user', [UserController::class, 'show']);
-        Route::apiResource('groups', GroupController::class);
-        Route::apiResource('parties', PartyController::class);
-        Route::apiResource('wallets', WalletController::class);
-        Route::apiResource('transfers', TransferController::class);
+    // Stateful authenticated routes
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::group(['middleware' => ['request.body.json']], function () {
+            Route::get('/user', [UserController::class, 'show']);
+            Route::apiResource('groups', GroupController::class);
+            Route::apiResource('parties', PartyController::class);
+            Route::apiResource('wallets', WalletController::class);
+            Route::apiResource('transfers', TransferController::class);
+        });
+        Route::apiResource('configurations', ConfigurationController::class);
+        Route::apiResource('transactions', TransactionController::class);
+        Route::post('/transactions/{id}/files', [TransactionController::class, 'uploadFiles']);
+        Route::delete('/transactions/{id}/files/{file_id}', [TransactionController::class, 'deleteFiles']);
+        Route::apiResource('categories', CategoryController::class);
+
+        // Import routes
+        Route::post('import', [ImportController::class, 'import']);
+        Route::get('imports', [ImportController::class, 'getImports']);
+        Route::get('imports/{id}/failed', [ImportController::class, 'getFailedImports']);
+        Route::put('imports/{id}/fix', [ImportController::class, 'fixFailedImports']);
     });
-    Route::apiResource('configurations', ConfigurationController::class);
-    Route::apiResource('transactions', TransactionController::class);
-    Route::post('/transactions/{id}/files', [TransactionController::class, 'uploadFiles']);
-    Route::delete('/transactions/{id}/files/{file_id}', [TransactionController::class, 'deleteFiles']);
-    Route::apiResource('categories', CategoryController::class);
-
-    // Import routes
-    Route::post('import', [ImportController::class, 'import']);
-    Route::get('imports', [ImportController::class, 'getImports']);
-    Route::get('imports/{id}/failed', [ImportController::class, 'getFailedImports']);
-    Route::put('imports/{id}/fix', [ImportController::class, 'fixFailedImports']);
 });
