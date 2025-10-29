b430c3702b33d470e3993593d74b41bebcb2ed5f
Kofi <kofimokome10@gmail.com>
2025-10-29 19:52:09 +0100

    refactor: Move VersionController to the API folder

--- app/Http/Controllers/API/VersionController.php
+++ app/Http/Controllers/API/VersionController.php
@@ -1,11 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\API\v1;
+namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\API\ApiController;
-use App\Http\Traits\ApiQueryable;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use OpenApi\Attributes as OA;
 
 /**
@@ -13,8 +10,6 @@ use OpenApi\Attributes as OA;
  */
 class VersionController extends ApiController
 {
-    use ApiQueryable;
-
     #[OA\Get(
         path: '/info',
         summary: 'Get server info',
@@ -25,16 +20,12 @@ class VersionController extends ApiController
                 description: 'Successful response',
             ),
             new OA\Response(
-                response: 401,
-                description: 'Unauthorized'
-            ),
-            new OA\Response(
                 response: 500,
                 description: 'Server error'
             ),
         ]
     )]
-    public function getServerInfo(Request $request): JsonResponse
+    public function getServerInfo(): JsonResponse
     {
         return $this->success(['version' => '1.0.0']);
     }
