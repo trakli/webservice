73e30039648506102fd78db75c91e39485400a6c
iMercy <mercybrightmassang@gmail.com>
2025-11-09 19:03:31 -0700

    feat: Package level whitelist support

--- config/allowed-config-keys.php
+++ config/allowed-config-keys.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'keys' => [
-        'default-wallet',
-        'default-currency',
-        'default-group',
-        'default-lang',
-        'onboarding-complete',
-        'theme',
-    ],
+    // 'keys' => [
+    //     'default-wallet',
+    //     'default-currency',
+    //     'default-group',
+    //     'default-lang',
+    //     'onboarding-complete',
+    //     'theme',
+    // ],
+    'keys' => explode(',', env('MODELS_ALLOWED_CONFIGS', 'default-wallet,default-currency,default-group,default-lang,onboarding-complete,theme')),
 ];
